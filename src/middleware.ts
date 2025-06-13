96b0e8fb94db9e2f1a6ab07ee705bcdb12210294
ArtmdX <arthur.mdx1234@gmail.com>
2025-06-12 21:33:52 -0300

    fix: :bug: Solving a redirect bug from login to dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/customers/:path*', '/travels/:path*']
+  matcher: ['/auth/login', '/dashboard/:path*', '/customers/:path*', '/travels/:path*']
 };
